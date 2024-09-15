06f48edfc369c304e6faa731bb173943c6d777a5
wesley-rodrigues-silva <168359330+devweslley@users.noreply.github.com>
2024-09-15 13:20:54 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
-const buttons = document.querySelectorAll('#image-picker li')
-const image = document.querySelector('#product-image')
+// Seleciona todos os elementos <li> dentro do elemento com o ID 'image-picker'
+const buttons = document.querySelectorAll('#image-picker li');
 
+// Seleciona o elemento com o ID 'product-image'
+const image = document.querySelector('#product-image');
+
+// Itera sobre cada botão (<li>) encontrado
 buttons.forEach((btn) => {
-    btn.addEventListener('click', (e) =>{
+    // Adiciona um ouvinte de evento para o clique em cada botão
+    btn.addEventListener('click', (e) => {
 
-    buttons.forEach((btn) =>{
-        btn.querySelector('.color').classList.remove('selected');
-    })
+        // Remove a classe 'selected' de todos os botões
+        buttons.forEach((btn) => {
+            btn.querySelector('.color').classList.remove('selected');
+        });
 
-    const button = e.target;
+        // Obtém o botão clicado a partir do evento
+        const button = e.target;
 
-    const id = button.getAttribute('id');
+        // Obtém o valor do atributo 'id' do botão clicado
+        const id = button.getAttribute('id');
 
-    button.querySelector('.color').classList.add('selected');
+        // Adiciona a classe 'selected' ao botão clicado
+        button.querySelector('.color').classList.add('selected');
 
-    image.classList.add('changing');
-    image.setAttribute('src', `img/iphone_${id}.jpg`);
+        // Adiciona a classe 'changing' à imagem para iniciar uma animação (por exemplo, um efeito de transição)
+        image.classList.add('changing');
+        
+        // Atualiza o atributo 'src' da imagem com base no ID do botão clicado, alterando a imagem exibida
+        image.setAttribute('src', `img/iphone_${id}.jpg`);
 
-    setTimeout(() =>{
-        image.classList.toggle('changing');
-    }, 200);
-
-    })
-})
+        // Remove a classe 'changing' após 200 milissegundos para terminar a animação
+        setTimeout(() => {
+            image.classList.remove('changing');
+        }, 200);
+    });
+});
